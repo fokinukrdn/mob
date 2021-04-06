7c52faee80349f96b40035f59d882ccb000d08d7
fokinukrdn <44501163+fokinukrdn@users.noreply.github.com>
2021-04-06 19:35:15 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,449 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+
+
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'IoT',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BluetoothApp(),
+    );
+  }
+}
+
+class BluetoothApp extends StatefulWidget {
+  @override
+  _BluetoothAppState createState() => _BluetoothAppState();
+}
+
+class _BluetoothAppState extends State<BluetoothApp> {
+
+  BluetoothState _bluetoothState = BluetoothState.UNKNOWN;
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  FlutterBluetoothSerial _bluetooth = FlutterBluetoothSerial.instance;
+  BluetoothConnection connection;
+
+  int _deviceState;
+
+  bool isDisconnecting = false;
+
+  Map<String, Color> colors = {
+    'onBorderColor': Colors.green,
+    'offBorderColor': Colors.red,
+    'neutralBorderColor': Colors.transparent,
+    'onTextColor': Colors.green[700],
+    'offTextColor': Colors.red[700],
+    'neutralTextColor': Colors.blue,
+  };
+
+
+  bool get isConnected => connection != null && connection.isConnected;
+  List<BluetoothDevice> _devicesList = [];
+  BluetoothDevice _device;
+  bool _connected = false;
+  bool _isButtonUnavailable = false;
+
+  @override
+  void initState() {
+    super.initState();
+    FlutterBluetoothSerial.instance.state.then((state) {
+      setState(() {
+        _bluetoothState = state;
+      });
+    });
+
+    _deviceState = 0;
+    enableBluetooth();
+
+
+    FlutterBluetoothSerial.instance
+        .onStateChanged()
+        .listen((BluetoothState state) {
+      setState(() {
+        _bluetoothState = state;
+        if (_bluetoothState == BluetoothState.STATE_OFF) {
+          _isButtonUnavailable = true;
+        }
+        getPairedDevices();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    if (isConnected) {
+      isDisconnecting = true;
+      connection.dispose();
+      connection = null;
+    }
+
+    super.dispose();
+  }
+
+
+  Future<void> enableBluetooth() async {
+
+    _bluetoothState = await FlutterBluetoothSerial.instance.state;
+    if (_bluetoothState == BluetoothState.STATE_OFF) {
+      await FlutterBluetoothSerial.instance.requestEnable();
+      await getPairedDevices();
+      return true;
+    } else {
+      await getPairedDevices();
+    }
+    return false;
+  }
+
+
+  Future<void> getPairedDevices() async {
+    List<BluetoothDevice> devices = [];
+
+
+    try {
+      devices = await _bluetooth.getBondedDevices();
+    } on PlatformException {
+      print("Error");
+    }
+    if (!mounted) {
+      return;
+    }
+    setState(() {
+      _devicesList = devices;
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        key: _scaffoldKey,
+        appBar: AppBar(
+          title: Text("IoT"),
+          backgroundColor: Colors.deepPurple,
+          actions: <Widget>[
+            FlatButton.icon(
+              icon: Icon(
+                Icons.refresh,
+                color: Colors.white,
+              ),
+              label: Text(
+                "Перезагрузить",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              splashColor: Colors.deepPurple,
+              onPressed: () async {
+                await getPairedDevices().then((_) {
+                  show('Device list refreshed');
+                });
+              },
+            ),
+          ],
+        ),
+        body: Container(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Visibility(
+                visible: _isButtonUnavailable &&
+                    _bluetoothState == BluetoothState.STATE_ON,
+                child: LinearProgressIndicator(
+                  backgroundColor: Colors.yellow,
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.red),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        'Включить Bluetooth',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    Switch(
+                      value: _bluetoothState.isEnabled,
+                      onChanged: (bool value) {
+                        future() async {
+                          if (value) {
+                            await FlutterBluetoothSerial.instance
+                                .requestEnable();
+                          } else {
+                            await FlutterBluetoothSerial.instance
+                                .requestDisable();
+                          }
+                          await getPairedDevices();
+                          _isButtonUnavailable = false;
+
+                          if (_connected) {
+                            _disconnect();
+                          }
+                        }
+                        future().then((_) {
+                          setState(() {});
+                        });
+                      },
+                    )
+                  ],
+                ),
+              ),
+              Stack(
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: Text(
+                          "Подключенные устройства",
+                          style: TextStyle(fontSize: 24, color: Colors.blue),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              'Устройства:',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            DropdownButton(
+                              items: _getDeviceItems(),
+                              onChanged: (value) =>
+                                  setState(() => _device = value),
+                              value: _devicesList.isNotEmpty ? _device : null,
+                            ),
+                            RaisedButton(
+                              onPressed: _isButtonUnavailable
+                                  ? null
+                                  : _connected ? _disconnect : _connect,
+                              child:
+                              Text(_connected ? 'Отключить' : 'Подключить'),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            side: new BorderSide(
+                              color: _deviceState == 0
+                                  ? colors['neutralBorderColor']
+                                  : _deviceState == 1
+                                  ? colors['onBorderColor']
+                                  : colors['offBorderColor'],
+                              width: 3,
+                            ),
+                            borderRadius: BorderRadius.circular(4.0),
+                          ),
+                          elevation: _deviceState == 0 ? 4 : 0,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: Text(
+                                    "DEVICE 1",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      color: _deviceState == 0
+                                          ? colors['neutralTextColor']
+                                          : _deviceState == 1
+                                          ? colors['onTextColor']
+                                          : colors['offTextColor'],
+                                    ),
+                                  ),
+                                ),
+                                FlatButton(
+                                  onPressed: _connected
+                                      ? _sendOnMessageToBluetooth
+                                      : null,
+                                  child: Text("ON"),
+                                ),
+                                FlatButton(
+                                  onPressed: _connected
+                                      ? _sendOffMessageToBluetooth
+                                      : null,
+                                  child: Text("OFF"),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "«Если вы не можете найти устройство, выполните сопряжение в настройках Bluetooth»",
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red,
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        RaisedButton(
+                          elevation: 2,
+                          child: Text("Настройка Bluetooth"),
+                          onPressed: () {
+                            FlutterBluetoothSerial.instance.openSettings();
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  List<DropdownMenuItem<BluetoothDevice>> _getDeviceItems() {
+    List<DropdownMenuItem<BluetoothDevice>> items = [];
+    if (_devicesList.isEmpty) {
+      items.add(DropdownMenuItem(
+        child: Text('NONE'),
+      ));
+    } else {
+      _devicesList.forEach((device) {
+        items.add(DropdownMenuItem(
+          child: Text(device.name),
+          value: device,
+        ));
+      });
+    }
+    return items;
+  }
+
+
+  void _connect() async {
+    setState(() {
+      _isButtonUnavailable = true;
+    });
+    if (_device == null) {
+      show('No device selected');
+    } else {
+      if (!isConnected) {
+        await BluetoothConnection.toAddress(_device.address)
+            .then((_connection) {
+          print('Connected to the device');
+          connection = _connection;
+          setState(() {
+            _connected = true;
+          });
+
+          connection.input.listen(null).onDone(() {
+            if (isDisconnecting) {
+              print('Disconnecting locally!');
+            } else {
+              print('Disconnected remotely!');
+            }
+            if (this.mounted) {
+              setState(() {});
+            }
+          });
+        }).catchError((error) {
+          print('Cannot connect, exception occurred');
+          print(error);
+        });
+        show('Device connected');
+
+        setState(() => _isButtonUnavailable = false);
+      }
+    }
+  }
+
+
+  void _disconnect() async {
+    setState(() {
+      _isButtonUnavailable = true;
+      _deviceState = 0;
+    });
+
+    await connection.close();
+    show('Device disconnected');
+    if (!connection.isConnected) {
+      setState(() {
+        _connected = false;
+        _isButtonUnavailable = false;
+      });
+    }
+  }
+
+
+  void _sendOnMessageToBluetooth() async {
+    connection.output.add(utf8.encode("1" + "\r\n"));
+    await connection.output.allSent;
+    show('Device Turned On');
+    setState(() {
+      _deviceState = 1; // device on
+    });
+  }
+
+
+  void _sendOffMessageToBluetooth() async {
+    connection.output.add(utf8.encode("0" + "\r\n"));
+    await connection.output.allSent;
+    show('Device Turned Off');
+    setState(() {
+      _deviceState = -1; // device off
+    });
+  }
+
+
+  Future show(
+      String message, {
+        Duration duration: const Duration(seconds: 3),
+      }) async {
+    await new Future.delayed(new Duration(milliseconds: 100));
+    _scaffoldKey.currentState.showSnackBar(
+      new SnackBar(
+        content: new Text(
+          message,
+        ),
+        duration: duration,
+      ),
+    );
+  }
+}
